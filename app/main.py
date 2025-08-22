525726828d4dbc9ad1b309765c771525a07a4dc8
piyush-c04 <143525614+piyush-c04@users.noreply.github.com>
2025-08-22 11:14:15 +0530

    created structure of code

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-from flask_jwt_extended import JWTManager
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-CORS(app)
