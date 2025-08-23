f1b7d44536dd7ab1d1f97d964ccce3688378264f
piyush-c04 <143525614+piyush-c04@users.noreply.github.com>
2025-08-23 20:30:47 +0530

    Cursor query fetched

--- app/checkdb.py
+++ app/checkdb.py
@@ -8,6 +8,13 @@ dburl = os.getenv("DATABASE_URL")
 try:
     conn = psycopg2.connect(dburl)
     print("Connection successful!")
+    # curr = conn.cursor()
+    # curr.execute("SELECT * from test;")
+    # rows = curr.fetchall()
+    # for row in rows:
+    #     print(row)
+    # curr.close()
+    
     conn.close()
 except Exception as e:
     print(f"Connection failed: {e}")
